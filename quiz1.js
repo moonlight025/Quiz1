6695418316ad4b1d531cd389e07a8a5f4cb9ad17
moonlight025 <tw2016905@gmail.com>
2021-03-29 19:34:56 +0800

    third

--- quiz1.js
+++ quiz1.js
@@ -1,7 +1,9 @@
 function change() {
-    document.getElementById('hid123').innerHTML = "CSIE@CGU";
-    document.getElementById('pid123').innerHTML = "怎麼那麼棒！！.";
-  }
+    // document.getElementById('hid123').innerHTML = "CSIE@CGU";
+    // document.getElementById('pid123').innerHTML = "怎麼那麼棒！！.";
+    $('#hid123').html('CSIE@CGU');
+    $('#pid123').html('怎麼那麼棒！！.');
+}
    function myFunction() {   
     var btn = document.createElement("BUTTON");   
     btn.innerHTML = "Change this document";
